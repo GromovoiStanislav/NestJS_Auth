7c33972183dd0a56fdf3d5d2c7eee698fca8e55d
Gromovoi Stanislav <gromila12@rambler.ru>
2023-05-10 15:07:26 +0600

    ModuleRef

--- auth_5/src/users/users.module.ts
+++ auth_5/src/users/users.module.ts
@@ -7,7 +7,7 @@ import { AuthModule } from 'src/auth/auth.module';
 @Module({
   imports: [
     PrismaModule,
-    forwardRef(() => AuthModule)
+    // forwardRef(() => AuthModule)
   ],
   providers: [UsersService],
   exports: [UsersService],
--- auth_5/src/users/users.service.ts
+++ auth_5/src/users/users.service.ts
@@ -1,20 +1,28 @@
 import {
   Injectable,
-  NotFoundException,
+  NotFoundException, OnModuleInit,
   UnprocessableEntityException
 } from "@nestjs/common";
 import { PasswordService } from "src/auth/services/password.service";
 import { PrismaService } from "src/prisma/prisma.service";
 import { CreateUserDto } from "./create-user.dto";
 import { UserDto } from "./user.dto";
+import { ModuleRef } from "@nestjs/core";
 
 @Injectable()
-export class UsersService {
+export class UsersService implements OnModuleInit {
+
+  private passwordService: PasswordService
 
   constructor(
     private readonly prismaService: PrismaService,
-    private passwordService: PasswordService
+    // private passwordService: PasswordService
+    private moduleRef: ModuleRef,
   ) {
+  }
+
+  onModuleInit() {
+    this.passwordService = this.moduleRef.get(PasswordService, { strict: false });
   }
 
 
